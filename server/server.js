0eef4588fa2c41a411481e3c712dc0657c68135a
Mehroz Khan <khanmehroz245@gmail.com>
2024-02-18 14:44:37 +0500

    Router & Controllers Setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+// server.js
+const express = require('express');
+const userRouter = require('./routers/userRouter');
+const profileRouter = require('./routers/profileRouter');
+const authRouter = require('./routers/authRouter');
+const workoutRouter = require('./routers/workoutRouter');
+const nutritionRouter = require('./routers/nutritionRouter');
+const progressRouter = require('./routers/progressRouter');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Use routers
+app.use('/api/user', userRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/workout', workoutRouter);
+app.use('/api/nutrition', nutritionRouter);
+app.use('/api/progress', progressRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
