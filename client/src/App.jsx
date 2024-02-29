68a1448c2a73f4bbacb233a8d127f8fb5580624c
Mehroz Khan <khanmehroz245@gmail.com>
2024-02-29 17:25:05 +0500

    Userdashboard 20% Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
      <BrowserRouter>
-        { <Navbar /> }
+        {location.pathname !== "/dashboard" && <Navbar />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
