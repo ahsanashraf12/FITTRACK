0d4f9a88f737f78e960566bc69c7a89b0477e006
Muhammad Ahsan <138646384+ahsanashraf12@users.noreply.github.com>
2024-03-06 22:47:46 +0500

    Profile 90% Work Complete

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,47 +1,46 @@
-// components/UserProfile.jsx
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import defaultProfileImage from '../assets/images/profile/user-1.jpg';
 
 export const UserProfile = () => {
   const { userId } = useParams();
-  console.log("UserId in UserDashboard:", userId);
+  const [user, setUser] = useState(null); // Initialize user state
 
   useEffect(() => {
     const fetchUserDetails = async () => {
       try {
         const response = await fetch(`http://localhost:5001/api/user/${userId}`);
         const userData = await response.json();
-        setUser(userData.user); // Assuming the user object is nested under "user" property
+        setUser(userData.user); // Update user state with fetched data
       } catch (error) {
         console.error('Error fetching user details:', error);
       }
     };
-  
+
     if (userId) {
-      fetchUserDetails(); // Corrected function name
+      fetchUserDetails();
     }
   }, [userId]);
 
   return (
     <>
-    <div>
-      {user ? (
-        <div>
-          <h2>{user.name}'s Profile</h2>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          {/* Other user details */}
-          {user.profilePhoto ? (
-            <img src={user.profilePhoto} alt={`${user.name}'s Profile`} />
-          ) : (
-            <img src={defaultProfileImage} alt="Default Profile" />
-          )}
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+      <div>
+        {user ? (
+          <div>
+            <h2>{user.name}'s Profile</h2>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+            {/* Other user details */}
+            {user.profilePhoto ? (
+              <img src={user.profilePhoto} alt={`${user.name}'s Profile`} />
+            ) : (
+              <img src={defaultProfileImage} alt="Default Profile" />
+            )}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </>
   );
 };
