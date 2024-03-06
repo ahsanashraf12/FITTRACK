d97e705c50baf98a7c1a31e7b5abc4693a02ab9d
Muhammad Ahsan <138646384+ahsanashraf12@users.noreply.github.com>
2024-03-06 21:51:01 +0500

    Profile Work Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
-import { UserDashboard } from "./pages/UserDashboard";
-import { UserProfile } from "./components/UserProfile"; // Import UserProfile
+import { UserDashboard } from "./pages/UserDashboard"; 
+import { UserProfile } from "./components/UserProfile"; // Import UserProfile from components
 
 function App() {
   return (
